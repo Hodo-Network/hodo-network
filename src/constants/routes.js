37a8d130afd3250d0c49e3931c98a6b34cde846a
nathanjessen <nathan.jessen@gmail.com>
2021-03-17 13:07:01 -0600

    Comment out unused variable

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -8,15 +8,15 @@ const COMING_SOON_ROUTE = "/";
 
 // Used to pull a convenient name for analytics tracking events. The key must
 // be react-router ready path, and can include params such as :id for popup windows
-const PATH_NAME_MAP = {
-  [DEFAULT_ROUTE]: "Home",
-  [BROWSE_ALL_ROUTE]: "Browse All",
-  [BROWSE_COUNTRIES_ROUTE]: "Browse Countries",
-  [BROWSE_CAPITALS_ROUTE]: "Browse Capitals",
-  [BROWSE_MONUMENTS_ROUTE]: "Browse Monuments",
-  [`${ASSET_ROUTE}/:id`]: `Asset Page`,
-  [COMING_SOON_ROUTE]: "Coming Soon",
-};
+// const PATH_NAME_MAP = {
+//   [DEFAULT_ROUTE]: "Home",
+//   [BROWSE_ALL_ROUTE]: "Browse All",
+//   [BROWSE_COUNTRIES_ROUTE]: "Browse Countries",
+//   [BROWSE_CAPITALS_ROUTE]: "Browse Capitals",
+//   [BROWSE_MONUMENTS_ROUTE]: "Browse Monuments",
+//   [`${ASSET_ROUTE}/:id`]: `Asset Page`,
+//   [COMING_SOON_ROUTE]: "Coming Soon",
+// };
 
 export {
   DEFAULT_ROUTE,
