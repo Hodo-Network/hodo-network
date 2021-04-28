30cbc43e418c3a9bde484efd509c301431631f7a
Nathan Jessen <nathanjessen@users.noreply.github.com>
2021-04-28 12:45:14 -0600

    Update capitals.ts

--- src/data/capitals.ts
+++ src/data/capitals.ts
@@ -4,7 +4,7 @@ export const capitals: Array<NFTProps> = [
     {
         id: 'BZTDWRaK6rQmnSLrrq4PANBeueYfDRFkpHV9KcMtjxL19wsJ1',
         img:
-            "https://images.unsplash.com/photo-1452838269443-c86e6a3134f9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+            "https://gateway.pinata.cloud/ipfs/QmWdDjeh7R5K9w7uzh7bSMgmuDoNTMYXVY5ER69LdHwWc4",
         name: "Cape Town",
         rarity: 'Epic',
         desc: 'Capital City of South Africa.',
@@ -22,7 +22,7 @@ export const capitals: Array<NFTProps> = [
     {
         id: 'BZTDWRaK6rQmnSLrrq4PANBeueYfDRFkpHV9KcMtjxL19wsJ3',
         img:
-            "https://images.unsplash.com/photo-1452838269443-c86e6a3134f9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+            "https://gateway.pinata.cloud/ipfs/QmWdDjeh7R5K9w7uzh7bSMgmuDoNTMYXVY5ER69LdHwWc4",
         name: "Jakarta",
         rarity: 'Rare',
         desc: 'Capital City of Indonesia.',
@@ -40,7 +40,7 @@ export const capitals: Array<NFTProps> = [
     {
         id: 'BZTDWRaK6rQmnSLrrq4PANBeueYfDRFkpHV9KcMtjxL19wsJ5',
         img:
-            "https://images.unsplash.com/photo-1452838269443-c86e6a3134f9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=512&q=80",
+            "https://gateway.pinata.cloud/ipfs/QmWdDjeh7R5K9w7uzh7bSMgmuDoNTMYXVY5ER69LdHwWc4",
         name: "Jamestown",
         rarity: 'Common',
         desc: 'Capital City of Saint Helena.',
