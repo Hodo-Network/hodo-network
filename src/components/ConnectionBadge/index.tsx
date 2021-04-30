f93d3308fe075019bceb75d4aa59e390f36ef28a
nathanjessen <nathan.jessen@gmail.com>
2021-04-29 22:57:06 -0600

    Animation for copied address

--- src/components/ConnectionBadge/index.tsx
+++ src/components/ConnectionBadge/index.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 import { NETWORK_LABELS_SHORT } from "../../constants";
 
 export default function ConnectionBadge() {
   const { chainId, account } = useWeb3React<Web3Provider>();
+  const [message, setMessage] = useState<string>("");
+  const [messageColor, setMessageColor] = useState<string>("");
+  const [showMsg, setShowMsg] = useState<boolean>(false);
+
   const copyToClipboard = () => {
     if (account) {
       navigator.clipboard.writeText(account);
-      alert("Copied to clipboard: " + account);
+      navigator.clipboard
+        .writeText(account)
+        .then(
+          function () {
+            setMessageColor("green-500");
+            setMessage("Copied!");
+          },
+          function () {
+            setMessageColor("red-500");
+            setMessage("Failed to copy.");
+          }
+        )
+        .then(() => {
+          setShowMsg(true);
+          setTimeout(() => {
+            setShowMsg(false);
+          }, 1200);
+        });
     }
   };
 
   return (
-    <div className='flex text-xs font-medium'>
+    <div className='flex text-xs font-medium relative'>
       <span className='inline-flex items-center px-3 py-1.5 rounded-l-md border border-green-500 bg-green-50 text-green-700'>
         <svg
           className='-ml-0.5 mr-1.5 h-2 w-2 text-green-500 flex-shrink-0'
@@ -28,6 +49,12 @@ export default function ConnectionBadge() {
         onClick={copyToClipboard}>
         <span className='truncate'>{account}</span>
       </span>
+      {showMsg && (
+        <span
+          className={`absolute bg-gray-900 opacity-90 w-full h-full left-0 top-0 font-semibold text-center px-3 py-1.5 text-${messageColor}`}>
+          {message}
+        </span>
+      )}
     </div>
   );
 }
