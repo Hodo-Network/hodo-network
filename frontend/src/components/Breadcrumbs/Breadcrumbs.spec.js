40d18181f0369ade461e9b859236660c2d3cc126
nathanjessen <nathan.jessen@gmail.com>
2021-06-21 22:30:51 -0500

    Update Breadcrumbs with storybook

--- frontend/src/components/Breadcrumbs/Breadcrumbs.spec.js
+++ frontend/src/components/Breadcrumbs/Breadcrumbs.spec.js
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom";
-import { cleanup } from "@testing-library/react";
 import renderer from "react-test-renderer";
-import Breadcrumbs from "./index";
+import { cleanup } from "@testing-library/react";
+import { Home } from "./Breadcrumbs.stories";
 
 afterEach(cleanup);
 
 test("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Breadcrumbs />, div);
+  ReactDOM.render(<Home {...Home.args} />, div);
 });
 
 test("matches snapshot", () => {
-  const tree = renderer.create(<Breadcrumbs />).toJSON();
+  const tree = renderer.create(<Home {...Home.args} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
