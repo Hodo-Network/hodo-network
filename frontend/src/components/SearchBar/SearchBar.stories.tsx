a15ce1603d0adc4c82d80ae4326bcaef58b6ace5
nathanjessen <nathan.jessen@gmail.com>
2021-06-21 23:29:49 -0500

    SearchBar story

---
+++ frontend/src/components/SearchBar/SearchBar.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import { SearchBar } from "./SearchBar";
+
+export default {
+  title: "Components/SearchBar",
+  component: SearchBar,
+} as Meta;
+
+const Template: Story = (args) => <SearchBar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
