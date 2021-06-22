a659e3f5a89e8297e39271a94c863964fc211a8a
nathanjessen <nathan.jessen@gmail.com>
2021-06-21 23:38:46 -0500

    Hero story

--- frontend/src/components/Hero/Hero.spec.js
+++ frontend/src/components/Hero/Hero.spec.js
@@ -1,16 +1,16 @@
-import { cleanup } from "@testing-library/react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
-import Hero from "./index";
+import { cleanup } from "@testing-library/react";
+import { Default } from "./Hero.stories";
 
 afterEach(cleanup);
 
 test("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Hero />, div);
+  ReactDOM.render(<Default {...Default.args} />, div);
 });
 
 test("matches snapshot", () => {
-  const tree = renderer.create(<Hero />).toJSON();
+  const tree = renderer.create(<Default {...Default.args} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
