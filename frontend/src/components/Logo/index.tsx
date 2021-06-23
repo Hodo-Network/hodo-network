f666a1c314fc3827424d7209ab7fe8d981696f30
nathanjessen <nathan.jessen@gmail.com>
2021-06-23 10:18:07 -0500

    Story for Logo

--- frontend/src/components/Logo/index.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { StyledLogo } from "./styles";
-
-const Logo: FC<{ img: string; alt: string }> = ({ img, alt }) => (
-  <StyledLogo src={img} alt={alt} className='h-6 w-auto' />
-);
-
-export default Logo;
