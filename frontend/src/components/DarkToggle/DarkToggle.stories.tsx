0f79b96b65fce33d56de58e6f9c79dec0437008c
nathanjessen <nathan.jessen@gmail.com>
2021-06-22 22:34:38 -0500

    Story for DarkToggle

---
+++ frontend/src/components/DarkToggle/DarkToggle.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import { DarkToggle } from "./DarkToggle";
+
+export default {
+  title: "Components/DarkToggle",
+  component: DarkToggle,
+} as Meta;
+
+const Template: Story = (args) => <DarkToggle {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
