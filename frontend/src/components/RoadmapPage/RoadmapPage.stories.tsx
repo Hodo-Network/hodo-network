9ecb872d1743f0ebacad21107aa81062f3ad9269
nathanjessen <nathan.jessen@gmail.com>
2021-06-23 14:08:46 -0500

    Updates for Roadmap

---
+++ frontend/src/components/RoadmapPage/RoadmapPage.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import { RoadmapPage } from "./RoadmapPage";
+
+export default {
+  title: "Pages/RoadmapPage",
+  component: RoadmapPage,
+} as Meta;
+
+const Template: Story = (args) => <RoadmapPage {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ frontend/src/components/RoadmapPage/RoadmapPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ContentWrapper from "../../ContentWrapper";
+import { RoadmapItems } from "../RoadmapItems";
+import { roadmap } from "../../data/roadmap";
+
+export const RoadmapPage = () => {
+  return (
+    <ContentWrapper>
+      <section
+        aria-labelledby='primary-heading'
+        className='p-4 sm:p-8 max-w-8xl'>
+        <h1 id='primary-heading' className='sr-only'>
+          Roadmap
+        </h1>
+
+        <RoadmapItems items={roadmap} />
+      </section>
+    </ContentWrapper>
+  );
+};
