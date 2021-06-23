8b127e5d6cbd6f46149278108bad8ad76f3a3b38
nathanjessen <nathan.jessen@gmail.com>
2021-06-23 10:24:15 -0500

    Story for LocationButton

---
+++ frontend/src/components/LocationButton/LocationButton.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import { LocationButton } from "./LocationButton";
+
+export default {
+  title: "Components/LocationButton",
+  component: LocationButton,
+} as Meta;
+
+const Template: Story = (args) => <LocationButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
