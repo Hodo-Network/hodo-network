da3d2c1ce582750c5c041a6498e0e93d8b610b48
nathanjessen <nathan.jessen@gmail.com>
2021-06-23 16:09:02 -0500

    Story for HomePage

---
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ContentWrapper from "../../ContentWrapper";
+import { Hero } from "../../components";
+
+export const HomePage = () => {
+  return (
+    <ContentWrapper>
+      <section
+        aria-labelledby='primary-heading'
+        className='px-4 sm:px-8 max-w-8xl'>
+        <h1 id='primary-heading' className='sr-only'>
+          Home
+        </h1>
+
+        <Hero />
+      </section>
+    </ContentWrapper>
+  );
+};
