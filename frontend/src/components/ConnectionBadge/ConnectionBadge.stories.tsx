f227805e35079f91b7fd77994afb4def3654b974
nathanjessen <nathan.jessen@gmail.com>
2021-06-22 22:29:07 -0500

    Story for ConnectionBadge

---
+++ frontend/src/components/ConnectionBadge/ConnectionBadge.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import { ConnectionBadge, ConnectionBadgeProps } from "./ConnectionBadge";
+import { ZERO_ADDRESS } from "../../constants";
+
+export default {
+  title: "Components/ConnectionBadge",
+  component: ConnectionBadge,
+} as Meta;
+
+const Template: Story<ConnectionBadgeProps> = (args) => (
+  <ConnectionBadge {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  network: "Ethereum",
+  account: ZERO_ADDRESS,
+};
