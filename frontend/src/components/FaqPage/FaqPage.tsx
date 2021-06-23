0d81d39afb1d7653fa17ae5319cb7869b4db3f8c
nathanjessen <nathan.jessen@gmail.com>
2021-06-23 14:16:06 -0500

    Story for FaqPage

---
+++ frontend/src/components/FaqPage/FaqPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { faqs } from "../../data/faqs";
+import ContentWrapper from "../../ContentWrapper";
+import { FaqList } from "../../components";
+
+export const FaqPage = () => {
+  return (
+    <ContentWrapper>
+      <div className='p-4 sm:p-8 max-w-8xl divide-y-2 divide-gray-200 dark:divide-gray-800'>
+        <h2 className='text-3xl sm:text-4xl font-extrabold text-gray-900 dark:text-white'>
+          Frequently Asked Questions
+        </h2>
+        <div className='mt-6'>
+          <FaqList items={faqs} />
+        </div>
+      </div>
+    </ContentWrapper>
+  );
+};
