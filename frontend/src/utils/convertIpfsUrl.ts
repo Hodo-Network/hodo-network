b7765899eb56052952c9978e213838be966eb302
nathanjessen <nathan.jessen@gmail.com>
2021-10-27 17:47:19 +0200

    Update metadata. Add ipfs url util

---
+++ frontend/src/utils/convertIpfsUrl.ts
@@ -0,0 +1,10 @@
+import IPFSGatewayTools from "@pinata/ipfs-gateway-tools/dist/browser";
+import { IPFS_GATEWAY_PREFIX } from "../constants";
+
+// @param url
+// ipfs://CID
+// @return https://gateway.pinata.cloud/ipfs/CID
+export default function convertIpfsUrl(url: string) {
+    const gatewayTools = new IPFSGatewayTools();
+    return gatewayTools.convertToDesiredGateway(url, IPFS_GATEWAY_PREFIX);
+};
