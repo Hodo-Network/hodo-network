8eec721680b4a8214af0e9a51c167f8e319f1893
nathanjessen <nathan.jessen@gmail.com>
2022-07-02 20:33:54 -0500

    Fix storybook build for webpack5

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,9 +1,12 @@
 module.exports = {
+  core: {
+    builder: "webpack5",
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
-    "storybook-tailwind-dark-mode",
   ],
+  staticDirs: ["../public"],
 };
